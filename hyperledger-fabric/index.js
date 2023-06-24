0c87c38b2f04fd4ad93bc65a4a3793189248e456
Namit Mani <56168455+NamitMani@users.noreply.github.com>
2023-06-23 21:19:49 -0400

    Delete hyperledger-fabric directory

--- hyperledger-fabric/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const balanceTransfer = require("./lib/balanceTransfer");
-const autorium = require("./lib/autorium");
-
-module.exports.BalanceTransfer = balanceTransfer;
-module.exports.Autorium = autorium;
-module.exports.contracts = [balanceTransfer, autorium];
